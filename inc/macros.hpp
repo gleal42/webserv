858ea2e20030a01c0e49d31950acd8c982936240
Manuel Sousa <mlrcbsousa@gmail.com>
2022-06-25 20:04:39 +0200

    Remove useless macro.

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 21:17:52 by gleal             #+#    #+#             */
-/*   Updated: 2022/06/25 19:31:05 by msousa           ###   ########.fr       */
+/*   Updated: 2022/06/25 20:04:08 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,5 @@
 
 # define LOG(x) std::cout << x << std::endl
 # define ERROR(x) std::cerr << x << std::endl
-
-# define TOSTRING(x) (static_cast<std::ostringstream*>( &((std::ostringstream()) << (x)) ))->str()
 
 #endif
