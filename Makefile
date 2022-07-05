b47a2af22fc14c5a84d468bbd8ccbcdabd73cce8
Manuel Sousa <mlrcbsousa@gmail.com>
2022-07-05 09:41:50 +0200

    Make it compile.

--- Makefile
+++ Makefile
@@ -10,6 +10,7 @@ SRCS := main.cpp \
 		Server.cpp \
 		Socket.cpp \
 		FileHandler.cpp \
+		BaseStatus.cpp \
 		webserver.cpp
 VPATH = src/
 OBJ_DIR := obj/
--- inc/webserver.hpp
+++ inc/webserver.hpp
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/22 16:09:11 by msousa            #+#    #+#             */
-/*   Updated: 2022/06/25 19:51:05 by msousa           ###   ########.fr       */
+/*   Updated: 2022/07/05 09:30:33 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,27 @@
 # include <string>
 # include <cstdlib> // For EXIT_FAILURE, etc, macros
 # include <sstream>
+# include <map>
 
 # include "macros.hpp"
 // Didn't work to include all header files here, if someone wants to give it
 // a shot whilst still being able to `make`
 
+typedef std::map<int, std::string> HTTPStatuses;
+
+enum HTTPStatusGroup {
+	STATUS_INVALID,
+	STATUS_INFO,
+	STATUS_SUCCESS,
+	STATUS_REDIRECT,
+	STATUS_CLIENT_ERROR,
+	STATUS_SERVER_ERROR,
+};
+
 // Functions
-int 		webserver(std::string config);
-std::string	to_string(int number);
+int 				webserver(std::string config);
+std::string			to_string(int number);
+std::string const	http_phrase( int code );
+HTTPStatusGroup		http_group( int code );
 
 #endif
