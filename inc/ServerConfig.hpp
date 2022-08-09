b287e098fc1dfb59f89562d10dded555d69f100d
coderMeira <https://coderMeira@github.com>
2022-08-09 19:01:19 +0100

    merging master with new config files

--- inc/ServerConfig.hpp
+++ inc/ServerConfig.hpp
@@ -1,32 +1,125 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ServerConfig.hpp                                     :+:      :+:    :+:   */
+/*   ServerConfig.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gleal <gleal@student.42lisboa.com>         +#+  +:+       +#+        */
+/*   By: fmeira <fmeira@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/07 21:16:23 by gleal             #+#    #+#             */
-/*   Updated: 2022/06/13 17:03:23 by gleal            ###   ########.fr       */
+/*   Created: 2022/07/22 00:50:11 by fmeira            #+#    #+#             */
+/*   Updated: 2022/08/09 18:36:16 by fmeira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
 
 #ifndef SERVERCONFIG_HPP
 # define SERVERCONFIG_HPP
 
-#include <string>
+# include <iostream>
+# include <string>
+# include <vector>
+# include <map>
+
+# define PORT_MAX   65535
+# define PORT_MIN   1
 
 // Taken from Server Config Parsing.
 // Each server will have these things
 
-struct ServerConfig
-{
-	std::string		name;
-	std::string		bind_address; // following pseudo-code
-	int				port;
-	int				max_clients;
-	int				request_timeout;
-	std::string		root;
-	int				client_max_body_size;
+enum autobool{
+    off,
+    on,
+    unset
 };
+
+typedef std::map<std::string, std::vector<int> > errorPage;
+
+class BaseConfig{
+    public:
+    // Constructor
+        BaseConfig();
+    // Destructor
+        virtual ~BaseConfig();
+
+    // Setters
+    virtual int                     find_directive(std::string &content) = 0;
+    virtual void                    set_directive(int directive, std::string &content) = 0;
+    void                            set_root(bool has_separators, std::string &content);
+    void                            set_autoindex(std::string &content);
+    void                            set_error_pages(std::string &content);
+    void                            set_max_body_size(int has_separators,std::string &content);
+    void                            set_indexes(std::string &content);
+
+    // Getters
+    std::string                     get_root( void );
+    autobool                        get_autoindex( void );
+    errorPage                       get_error_pages( void );
+    int                             get_max_body_size( void );
+    std::vector<std::string>        get_indexes( void );
+
+    protected:
+        std::string                 _root;
+        autobool                    _autoindex;
+        errorPage                   _error_pages;
+        int                         _client_max_body_size;
+        std::vector<std::string>    _indexes;
+
+};
+
+class LocationConfig : public BaseConfig
+{
+    public:
+        LocationConfig();
+        ~LocationConfig();
+
+        bool                        is_empty( void );
+        // Setters
+        int                         find_directive(std::string &content);
+        void                        set_directive(int directive, std::string &content);
+        void                        set_cgi(std::string &content);
+        void                        set_limit_except(std::string &content);
+
+        // Getters
+        std::string                 get_cgi( void );
+        std::vector<std::string>    get_limit_except( void );
+
+    private:
+        std::string                 _cgi;
+        std::vector<std::string>    _limit_except;
+
+};
+typedef std::map<std::string, LocationConfig> Locations;
+
+class ServerConfig : public BaseConfig
+{
+    public:
+        ServerConfig();
+        ~ServerConfig();
+        ServerConfig(ServerConfig&);
+
+        bool                        is_empty( void );
+
+        // Setters
+        int                         find_directive(std::string &content);
+        void                        set_directive(int directive, std::string &content);
+        void                        set_listen(int has_separators, std::string &content);
+        void                        set_server_name(std::string &content);
+
+        // Getters
+        std::string                 get_ip( void );
+        int                         get_port( void );
+        Locations                   get_locations( void );
+        std::vector<std::string>    get_server_name( void );
+
+    private:
+        std::string                 _ip;
+        int                         _port;
+        std::vector<std::string>    _server_name;
+        Locations                   _locations;
+};
+
+typedef std::vector<ServerConfig> Configs;
+ServerConfig& operator= (const ServerConfig&);
+std::ostream&   operator<<(std::ostream&, ServerConfig&);
 
 #endif
--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -1,51 +1,185 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ConfigParser.cpp                                         :+:      :+:    :+:   */
+/*   ConfigParser.cpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
+/*   By: fmeira <fmeira@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/22 08:37:16 by msousa            #+#    #+#             */
-/*   Updated: 2022/06/23 09:55:21 by msousa           ###   ########.fr       */
+/*   Created: 2022/07/22 00:49:53 by fmeira            #+#    #+#             */
+/*   Updated: 2022/08/09 15:08:31 by fmeira           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ConfigParser.hpp"
 
-/* Constructors */
-ConfigParser::ConfigParser( void ) : _config_file(""), _configs_amount(0) { /* no-op */ }
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Exceptions~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+ConfigurationFileError::ConfigurationFileError(void)
+    : std::runtime_error("Error while opening configuration file")
+{ /* No-op */}
+
+ConfigurationDirectiveError::ConfigurationDirectiveError(const std::string err)
+    : std::runtime_error("Error: " + err + " directive is invalid")
+{ /* No-op */}
+
+ConfigurationSyntaxError::ConfigurationSyntaxError()
+    : std::runtime_error("Error. Invalid syntax on the configuration file")
+{ /* No-op */}
+
+LocationPathError::LocationPathError(const std::string err)
+    : std::runtime_error("Error. The following path isn't accessible: " + err)
+{ /* No-op */}
+
+NestedContextError::NestedContextError(void)
+    : std::runtime_error("Error trying to nest a context block")
+{ /* No-op */}
+
+EmptyContextBlockError::EmptyContextBlockError(void)
+    : std::runtime_error("Error trying to add an empty context block")
+{ /* No-op */}
+
+DirectiveOutOfScopeError::DirectiveOutOfScopeError(const std::string err)
+    : std::runtime_error("Error: " + err + ". All declarations must be inside server scopes")
+{ /* No-op */}
+
+OpenContextBlockError::OpenContextBlockError(void)
+    : std::runtime_error("Error: missing closing bracket (}) on context")
+{ /* No-op */}
+
+LocationURIError::LocationURIError(void)
+    : std::runtime_error("Error: missing location URI")
+{ /* No-op */}
+
+BadDirectoryError::BadDirectoryError(const std::string err)
+    : std::runtime_error("Error: " + err + ". Please provide a valid directory")
+{ /* No-op */}
+
+MultipleArgumentsError::MultipleArgumentsError(const std::string err)
+    : std::runtime_error("Error: " + err + ". This directive can only have one argument")
+{ /* No-op */}
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~Constructors~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+ConfigError::ConfigError() : std::runtime_error("Error in configuration"){};
+ConfigParser::ConfigParser(std::string config_file) : _config_file(config_file){ /* no-op */ }
 ConfigParser::ConfigParser( ConfigParser const & src ) { *this = src; }
-ConfigParser::ConfigParser(std::string config_file) : _config_file(config_file),
-	_configs_amount(0) { /* no-op */ }
 
-// Getters
-ServerConfig	ConfigParser::config( int const index ) const { return _configs[index]; }
-int	ConfigParser::configs_amount( void ) const { return _configs_amount; }
+ConfigParser::~ConfigParser(){};
 
-/* Destructor */
-ConfigParser::~ConfigParser( void ) { /* no-op */ }
 
-/* Assignment operator */
-ConfigParser &	ConfigParser::operator = ( ConfigParser const & rhs )
-{
-	if (this != &rhs) {
-		//value = rhs.value;
-	}
-	return *this;
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~Config-parsing utils~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+namespace {
+    std::string &strtrim(std::string &str)
+    {
+        str.erase(str.find_last_not_of(SEPARATORS) + 1);
+        str.erase(0, str.find_first_not_of(SEPARATORS));
+        return (str);
+    };
 }
 
-void	ConfigParser::call( void )
+/*~~~~~~~~~~~~~~~~~~~~~~~ConfigParser member functions~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// const ServerConfig   ConfigParser::config( int const index ) const { return (server_configs[index]); }
+
+int    ConfigParser::configs_amount( void ) const { return (server_configs.size()); }
+
+// This function parses a context block.
+// There are two possible contexts:
+//    - Server context (Inside a server Scope) [server {...}]
+//    - Location context (inside Location scope, which must always be inside a server Scope) [server {... location {...}}]
+// It will parse all the directives inside the context block, line by line, one directive per line
+void    ConfigParser::context_parser(std::ifstream *file, int context, std::string location_path, ServerConfig* server_ptr)
 {
-	// TODO: validate file exists or throw error
-	// TODO: validate file contents or throw error
-	// TODO: count server blocks in file
-	_configs_amount = 1; // Temporary
-	_configs = Configs(_configs_amount);
+    std::string     line;
+    std::string     directive;
+    std::string     content;
+    ServerConfig    new_server;
+    LocationConfig  new_location;
+    size_t          separator;
 
-	// Temp
-	_configs[0].port = 8080;
-	_configs[0].max_clients = 10;
-	_configs[0].client_max_body_size = 10000;
-	_configs[0].name = "Hello";
+    while (std::getline(*file, line))
+    {
+        line = strtrim(line);
+        if (!line.length() || line[0] == '#')
+            continue;
+        if (line == "}"){
+            if (context == SERVER_CONTEXT){
+                if (new_server.is_empty())
+                    throw (EmptyContextBlockError());
+                this->server_configs.push_back(new_server);
+            }
+            else if (context == LOCATION_CONTEXT){
+                if (new_location.is_empty())
+                    throw (EmptyContextBlockError());
+                if (is_directory(location_path))
+                    server_ptr->get_locations()[location_path] = new_location;
+            }
+            return;
+        }
+        separator = line.find_first_of(SEPARATORS);
+        directive = line.substr(0, separator);
+        content = line.substr(separator + 1);
+        std::cout << std::left << (context == LOCATION_CONTEXT ? "Location" : "Server") << " context: " << "[" << directive << "]" << " - [" << content << "]\n";
+        if (directive == "server")
+            throw (NestedContextError());
+        else if (directive == "location"){
+            separator = content.find_first_of("{");
+            if (separator == std::string::npos)
+                throw (ConfigurationSyntaxError());
+            else if (separator < 2)
+                throw (LocationURIError());
+            else if (context == LOCATION_CONTEXT)
+                throw (NestedContextError());
+            content = content.substr(0, separator - 1);
+            context_parser(file, LOCATION_CONTEXT, content, &new_server);
+        }
+        else{
+            if (content[content.length() - 1] != ';')
+                throw (ConfigurationSyntaxError());
+            content = content.substr(0, content.length() - 1);
+            if (context == SERVER_CONTEXT)
+                new_server.set_directive((new_server.find_directive(directive)), content);
+            else if (context == LOCATION_CONTEXT)
+                new_location.set_directive((new_location.find_directive(directive)), content);
+        }
+    }
+    throw (OpenContextBlockError());
+};
 
-}
+// This function opens the _config_file, and then parses the file while looking for an opening server block (aka "server {")
+// Then, calls context_parser() to parse the server block
+// OBS: So far, the new ServerConfig object is being stored inside the ConfigParser
+void    ConfigParser::call()
+{
+    std::ifstream   file;
+    std::string     line;
+    std::string     directive;
+    size_t          separator;
+
+    try{
+        file.open(this->_config_file.c_str());
+        if (!file.is_open())
+            throw ConfigurationFileError();
+        while (std::getline(file, line))
+        {
+            line = strtrim(line);
+            if (!line.length() || line[0] == '#')
+                continue;
+            separator = line.find_first_of(SEPARATORS);
+            directive = line.substr(0, separator);
+            if (directive == "server"){
+                if (line[separator + 1] != '{')
+                    throw ConfigurationSyntaxError();
+                context_parser(&file, SERVER_CONTEXT);
+            }
+            else
+                throw DirectiveOutOfScopeError(directive);
+        }
+    }
+    catch (ConfigError &e) {
+		    ERROR(e.what());
+    }
+    file.close();
+};
