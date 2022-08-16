ca074b587a42f78803e32dc2cd3eca4cbed6dfe4
Goncalo Leal <jobsgoncaloleal@gmail.com>
2022-08-16 17:30:06 +0100

    Added Dockerfile to test freds's PR

---
+++ Dockerfile_webserv
@@ -0,0 +1,15 @@
+# https://hub.docker.com/_/gcc
+FROM gcc:latest
+
+# Create app directory
+RUN mkdir -p /usr/src/webserv
+WORKDIR /usr/src/webserv
+
+# Copy source files
+COPY . /usr/src/webserv
+
+# Compile
+RUN make
+
+# Run
+CMD ["./webserv"]
--- deploy_freds.sh
+++ deploy_freds.sh
@@ -1,14 +1,14 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    compare_with_nginx.sh                              :+:      :+:    :+:    #
+#    deploy_freds.sh                                    :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: gleal <gleal@student.42lisboa.com>         +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/16 15:22:00 by gleal             #+#    #+#              #
-#    Updated: 2022/08/16 15:23:24 by gleal            ###   ########.fr        #
+#    Updated: 2022/08/16 15:24:33 by gleal            ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-docker build -t nginx_server .
-docker run --rm -it --name ft_server -p 80:80 nginx_server
+docker build -f Dockerfile_webserv -t fred_server .
+docker run --rm -it --name fred_server -p 80:80 fred_server
