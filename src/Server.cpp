20503591eb223a5a3c51ec6a4b15c6a271d02ce6
Manuel Sousa <mlrcbsousa@gmail.com>
2022-08-27 15:16:56 +0200

    Fix tabbing.

--- src/Server.cpp
+++ src/Server.cpp
@@ -6,7 +6,7 @@
 /*   By: msousa <mlrcbsousa@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/05 09:45:56 by msousa            #+#    #+#             */
-/*   Updated: 2022/08/27 14:59:38 by msousa           ###   ########.fr       */
+/*   Updated: 2022/08/27 15:16:45 by msousa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,9 +33,9 @@ size_t	Server::listeners_amount( void ) const { return _listeners_amount; }
 
 Server::Server(const ConfigParser &parser)
 {
-    _queue_fd = QUEUE();
-    if (_queue_fd < 0) {
-        throw CreateError();
+	_queue_fd = QUEUE();
+	if (_queue_fd < 0) {
+		throw CreateError();
 	}
 
 	_listeners_amount = parser.configs_amount();
@@ -65,46 +65,46 @@ void	Server::start( void )
 {
 	int n = 0;
 
-    while (1) {
+	while (1) {
 		n = events_wait();
 		LOG("Number of events recorded: " << n);
 
-        if (n <= 0) {
-            continue;
+		if (n <= 0) {
+			continue;
 		}
 
-        for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) {
 			Event		event(events[i]);
-            Listener_it	it = _listeners.find(event.fd());
+			Listener_it	it = _listeners.find(event.fd());
 
-            if (it != _listeners.end()) {
+			if (it != _listeners.end()) {
 				// New event for non-existent file descriptor
-                connection_new(it->second);
+				connection_new(it->second);
 			}
 			else {
 				Connections_it	connection_it = _connections.find(event.fd());
 
 				if (events[i].flags & EV_EOF) { // <--------
 					// If there are no more connections open in server do cleanup(return)
-                    connection_close(event.fd());
+					connection_close(event.fd());
 
-                    if (_connections.size() == 0) {
-                        return ;
+					if (_connections.size() == 0) {
+						return ;
 					}
-                }
+				}
 				else if (event.is_read()) {
-                    connection_read(connection_it->second, events[i].data); // <--------
+					connection_read(connection_it->second, events[i].data); // <--------
 				}
 				else if (event.is_write()) {
 					connection_write(connection_it->second);
 
-                    if (connection_it->second->response.is_empty()) {
-                        connection_it->second->request.clear();
+					if (connection_it->second->response.is_empty()) {
+						connection_it->second->request.clear();
 					}
-                }
-            }
-        }
-    }
+				}
+			}
+		}
+	}
 }
 
 int	Server::events_wait( void )
@@ -118,7 +118,7 @@ int	Server::events_wait( void )
 #if defined(LINUX)
 	events_ready = epoll_wait(_queue_fd, events, EVENTS_SIZE, TIMEOUT);
 #endif
-    return events_ready;
+	return events_ready;
 }
 
 void	Server::connection_new( Listener * listener )
@@ -187,8 +187,8 @@ void	Server::connection_new( Listener * listener )
  **/
 void	Server::connection_read( Connection *connection, int read_size )
 {
-    LOG("About to read the file descriptor: " << connection->fd());
-    LOG("Incoming data has size of: " << read_size);
+	LOG("About to read the file descriptor: " << connection->fd());
+	LOG("Incoming data has size of: " << read_size);
 
 	// For stream-oriented files (e.g., pipe, FIFO, stream socket),
 	// the condition that the read/write I/O space is exhausted can
@@ -203,32 +203,32 @@ void	Server::connection_read( Connection *connection, int read_size )
 	//
 	// TODO: how to get the `read_size` when using epoll?
 
-    connection->request.parse(*connection->socket(), read_size); // <--------
+	connection->request.parse(*connection->socket(), read_size); // <--------
 
-    if (connection->request._headers.count("Content-Length")) {
-        LOG("Analyzing if whole body was transferred: ");
+	if (connection->request._headers.count("Content-Length")) {
+		LOG("Analyzing if whole body was transferred: ");
 
-        std::stringstream	content_length(connection->request._headers["Content-Length"]);
-        size_t				value = 0;
+		std::stringstream	content_length(connection->request._headers["Content-Length"]);
+		size_t				value = 0;
 
-        content_length >> value;
+		content_length >> value;
 
-        if (connection->request._raw_body.size() < value + 1) {
-            LOG("Body total received :" << connection->request._raw_body.size());
-            LOG("Content Length :" << value);
-            LOG("Remaining :" << value - connection->request._raw_body.size());
+		if (connection->request._raw_body.size() < value + 1) {
+			LOG("Body total received :" << connection->request._raw_body.size());
+			LOG("Content Length :" << value);
+			LOG("Remaining :" << value - connection->request._raw_body.size());
 
 			return ;
-        }
-    }
+		}
+	}
 
 	LOG("|--- Headers ---|");
 	LOG(connection->request._raw_headers);
 	LOG("|--- Headers ---|");
-    if (connection->request._raw_body.size()) {
-        LOG("Final Body size :" << connection->request._raw_body.size());
-        LOG("Body :" << connection->request._raw_body.data());
-    }
+	if (connection->request._raw_body.size()) {
+		LOG("Final Body size :" << connection->request._raw_body.size());
+		LOG("Body :" << connection->request._raw_body.data());
+	}
 
 	connection_event_toggle_write(connection->fd());
 }
@@ -238,17 +238,17 @@ void	Server::connection_write( Connection *connection )
 	LOG("About to write to file descriptor: " << connection->fd());
 	LOG("The socket has the following size to write " << events[0].data); // <--------
 
-    if (connection->response.is_empty()) {
-        service(connection->request, connection->response);
+	if (connection->response.is_empty()) {
+		service(connection->request, connection->response);
 	}
 
-    connection->response.send_response(*connection->socket());
+	connection->response.send_response(*connection->socket());
 
-    if (connection->response.is_empty()) {
-        LOG("Connection was empty after sending");
+	if (connection->response.is_empty()) {
+		LOG("Connection was empty after sending");
 
 		connection_event_toggle_read(connection->fd());
-    }
+	}
 }
 
 void	Server::connection_event_toggle_write( int connection_fd )
@@ -293,30 +293,30 @@ void	Server::connection_event_toggle_read( int connection_fd )
 
 void	Server::service(Request & req, Response & res)
 {
-    FileHandler	handler;
-    try {
-        handler.service(req, res);
-    }
+	FileHandler	handler;
+	try {
+		handler.service(req, res);
+	}
 	catch (BaseStatus &error_status) {
-        res.set_error_body(error_status.code);
-        res.build_message(error_status);
-    }
+		res.set_error_body(error_status.code);
+		res.build_message(error_status);
+	}
 }
 
 Server::~Server()
 {
 	for (Listener_it it = _listeners.begin(); it != _listeners.end(); ++it) {
-        listener_close(it->first);
+		listener_close(it->first);
 	}
 	for (Connections_it it = _connections.begin(); it != _connections.end(); it++) {
-        connection_close(it->first);
+		connection_close(it->first);
 	}
-    close(_queue_fd);
+	close(_queue_fd);
 }
 
 void	Server::listener_close( int listener_fd )
 {
-    LOG("Closing Listener with fd: " << listener_fd);
+	LOG("Closing Listener with fd: " << listener_fd);
 
 #if defined(DARWIN)
 	EVENT event;
@@ -328,16 +328,16 @@ void	Server::listener_close( int listener_fd )
 	epoll_ctl(_queue_fd, EPOLL_CTL_DEL, listener_fd, NULL);
 #endif
 
-    delete _listeners[listener_fd];
+	delete _listeners[listener_fd];
 	// Weird! When I change this to _listeners.erase(listener_fd),
 	// which should be the correct one afaik
 	// I get a segfault
-    _connections.erase(listener_fd);
+	_connections.erase(listener_fd);
 }
 
 void	Server::connection_close( int connection_fd )
 {
-    LOG("Closing Connection: " << connection_fd);
+	LOG("Closing Connection: " << connection_fd);
 
 #if defined(DARWIN)
 	EVENT event;
@@ -351,13 +351,13 @@ void	Server::connection_close( int connection_fd )
 	epoll_ctl(_queue_fd, EPOLL_CTL_DEL, connection_fd, NULL);
 #endif
 
-    delete _connections[connection_fd];
-    _connections.erase(connection_fd);
+	delete _connections[connection_fd];
+	_connections.erase(connection_fd);
 }
 
 void Server::listener_event_read_add(int listener_fd)
 {
-    EVENT event;
+	EVENT event;
 
 #if defined(DARWIN)
 	EV_SET(&event, listener_fd, EVFILT_READ, EV_ADD, 0, 0, NULL);
